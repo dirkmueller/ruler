0f31709d604389a8ffdc2780c81c3ef77ed9ff19
Dirk Mueller <dirk@dmllr.de>
2015-05-19 18:39:19 +0200

    Initial state for compute

---
+++ compute/config.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# vim: sw=4 et
+#================
+# FILE          : config.sh
+#----------------
+# PROJECT       : OpenSuSE KIWI Image System
+# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
+#               :
+# AUTHOR        : Marcus Schaefer <ms@suse.de>
+#               :
+# BELONGS TO    : Operating System images
+#               :
+# DESCRIPTION   : configuration script for SUSE based
+#               : operating systems
+#               :
+#               :
+# STATUS        : BETA
+#----------------
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+cat > /etc/sysconfig/network/ifcfg-eth0 <<EOF
+BOOTPROTO='static'
+BROADCAST=''
+ETHTOOL_OPTIONS=''
+IPADDR='0.0.0.0/32'
+MTU=''
+NAME='Ethernet Card 0'
+NETMASK=''
+NETWORK=''
+REMOTE_IPADDR=''
+STARTMODE='auto'
+USERCONTROL='no'
+EOF
+
+ln -sf SLES.prod /etc/products.d/baseproduct
+
+echo cleaning hostname
+echo -n > /etc/HOSTNAME
+
+chmod +x /etc/init.d/after.local
+chmod +x /etc/init.d/compute
